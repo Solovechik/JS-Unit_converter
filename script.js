bde1ac04371b3acf0df54a3c63e01864853f410c
Solovechik <98522212+Solovechik@users.noreply.github.com>
2022-03-11 13:32:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ num.addEventListener('input', kiloPound);
 
 
 function meterFeet() {
-    feet = num.value * 3.28084;
-    meters = num.value * 0.3048;
-    result = `${num.value} meters = ${feet.toFixed(3)} feet | ${num.value} feet = ${meters.toFixed(3)} meters`;
+    let feet = num.value * 3.28084;
+    let meters = num.value * 0.3048;
+    let result = `${num.value} meters = ${feet.toFixed(3)} feet | ${num.value} feet = ${meters.toFixed(3)} meters`;
     document.getElementById('meterFeet').textContent = result;
 }
 
 function literGallon() {
-    gallons = num.value * 0.264172;
-    liters = num.value * 3.78541;
-    result = `${num.value} liters = ${gallons.toFixed(3)} gallons | ${num.value} gallons = ${liters.toFixed(3)} liters`;
+    let gallons = num.value * 0.264172;
+    let liters = num.value * 3.78541;
+    let result = `${num.value} liters = ${gallons.toFixed(3)} gallons | ${num.value} gallons = ${liters.toFixed(3)} liters`;
     document.getElementById('literGallon').textContent = result;
 }
 
 function kiloPound() {
-    pounds = num.value * 2.20462;
-    kilos = num.value * 0.453592;
-    result = `${num.value} kilos = ${pounds.toFixed(3)} pounds | ${num.value} pounds = ${kilos.toFixed(3)} kilos`;
+    let pounds = num.value * 2.20462;
+    let kilos = num.value * 0.453592;
+    let result = `${num.value} kilos = ${pounds.toFixed(3)} pounds | ${num.value} pounds = ${kilos.toFixed(3)} kilos`;
     document.getElementById('kiloPound').textContent = result;
 }
